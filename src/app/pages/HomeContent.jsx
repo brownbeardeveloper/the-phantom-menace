588362cca5df9157bfe50cae7b7670dfa48967a0
Björn Bergström <bjorn.wiklund.bergstrom@outlook.com>
2024-09-19 23:12:04 +0200

    Reorganized the basic file structure

---
+++ src/app/pages/HomeContent.jsx
@@ -0,0 +1,8 @@
+export default function HomeContent() {
+    return (
+        <main className="">
+            <h1 className="font-bold text-xl">It's working!</h1>
+        </main>
+    );
+  }
+  
