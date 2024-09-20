de848c429b04d3a63eed2b96d1833cef23c61474
Björn Bergström <bjorn.wiklund.bergstrom@outlook.com>
2024-09-21 01:15:19 +0200

    Completed the project's template

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,4 +1,4 @@
-import { createClient } from "../../../utils/supabase/server";
+import { createClient } from "../../services/supabase/server";
 
 export default async function Test() {
     const supabase = createClient();
