43e79d618e155dd59fde92a9f2b5d8c40bb3c82d
Björn Bergström <bjorn.wiklund.bergstrom@outlook.com>
2024-09-20 14:47:45 +0200

    Fixed the issue with the build

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main className="">
-            <h1 className="font-bold text-xl">It's working!</h1>
+            <h1 className="font-bold text-xl">It&apos;s working!</h1>
         </main>
     );
   }
