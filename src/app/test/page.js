01b0cf2672c678e1c2455aac14f0541f847db1bc
Johan Ahlsson <jjohan91@hotmail.com>
2024-09-20 14:59:31 +0100

    Supabase integrated

---
+++ src/app/test/page.js
@@ -0,0 +1,12 @@
+import { createClient } from "../../../utils/supabase/server";
+
+export default async function Test() {
+    const supabase = createClient();
+    const { data: notes, error } = await supabase.from('todos').select('*');
+
+    if (error) {
+        return <pre>Error fetching notes: {JSON.stringify(error, null, 2)}</pre>;
+    }
+
+    return <pre>{JSON.stringify(notes, null, 2)}</pre>;
+  }
